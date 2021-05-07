82e77419b3842c0f7d494873fb770543d9ece9e2
inzanty <nosko.nazar@gmail.com>
2021-05-08 00:58:11 +0300

    Admins checker

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# telegram-linux-daemon-bot
+# telegram-linux-command-bot
 Adds ability to manage your server from Telegram bot
 
 # Installation
 Install python-dependency ```telebot```.
 
 Fill your token from ```@BotFather``` to ```config.json``` in section ```token```
+
+Setup admins in ``config.json``. You can get your account id via ```@userinfobot```
 
 Start bot: ```python3 bot.py```
--- bot.py
+++ bot.py
@@ -2,8 +2,9 @@ import json
 import subprocess
 import telebot
 
-configFile = open('config.json')
-config = json.load(configFile)
+config_file = open('config.json')
+config = json.load(config_file)
+admin_ids = config["admin_ids"]
 bot = telebot.TeleBot(config["token"])
 
 # Get argument from command and convert from list to string
@@ -13,14 +14,15 @@ def extract_arg(arg):
 
 @bot.message_handler(commands=['command'])
 def exec_command(message):
-    argument = extract_arg(message.text)
-    if argument != "":
-        try:
-            command_response = subprocess.Popen(argument, shell=False, stdout=subprocess.PIPE).stdout.read()
-            bot.reply_to(message, command_response)
-        except FileNotFoundError as error:
-            bot.reply_to(message, error)
-    else:
-        bot.reply_to(message, "Command argument is empty")
+    if str(message.from_user.id) in admin_ids:
+        argument = extract_arg(message.text)
+        if argument != "":
+            try:
+                command_response = subprocess.Popen(argument, shell=False, stdout=subprocess.PIPE).stdout.read()
+                bot.reply_to(message, command_response)
+            except FileNotFoundError as error:
+                bot.reply_to(message, error)
+        else:
+            bot.reply_to(message, "Command argument is empty")
 
 bot.polling()
